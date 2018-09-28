f038aa8d4a477e13b883db8cfaedc640a2f1e7f7
gintoki05 <ajieprastyo@gmail.com>
2018-09-28 08:19:34 +0700

    add bug

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,7 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -7,6 +9,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
       auth: authReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
